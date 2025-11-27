fbe1adaeecf7be12b2fe2baaf3a50ead389f4420
sua <sua2713806@naver.com>
2025-11-27 21:11:09 +0900

    feat: 커뮤니티 피드 서비스 초안 설계하기(유저)
    - 유저 생성(id, name, profileImageUrl)
    - 팔로우 기능(count)

---
+++ src/user/domain/User.java
@@ -0,0 +1,58 @@
+package user.domain;
+
+import java.util.Objects;
+
+public class User {
+    private final long id;
+    private final UserInfo info;
+    private final UserRelationCounter followingCounter;
+    private final UserRelationCounter followerCounter;
+
+    public User(long id, UserInfo userInfo) {
+        this.id = id;
+        this.info = userInfo;
+        this.followingCounter = new UserRelationCounter();
+        this.followerCounter = new UserRelationCounter();
+    }
+
+    public void follow(User targetUser) {
+        if(targetUser.equals(this)) {
+            throw new IllegalArgumentException();
+        }
+
+        followingCounter.increase();
+        increaseFollowerCounter();
+
+    }
+
+    public void unfollow(User targetUser) {
+        if(this.equals(targetUser)) {
+            throw new IllegalArgumentException();
+        }
+
+        followingCounter.decrease();
+        decreaseFollowerCounter();
+    }
+
+    private void increaseFollowerCounter() {
+        followerCounter.increase();
+    }
+
+    private void decreaseFollowerCounter() {
+        followerCounter.decrease();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/user/domain/UserInfo.java
@@ -0,0 +1,17 @@
+package user.domain;
+
+public class UserInfo {
+
+    private final String name;
+    private final String profileImageUrl;
+
+    public UserInfo(String name, String profileImageUrl) {
+        if(name==null || name.isEmpty()) {
+            throw new IllegalArgumentException("name is null or empty");
+        }
+
+        this.name = name;
+        this.profileImageUrl = profileImageUrl;
+    }
+
+}
